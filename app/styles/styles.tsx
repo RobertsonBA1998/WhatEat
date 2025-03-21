cdcf43fd1cdf1d5bfd7d18e39fd7075335b36cbb
RobertsonBA <113301827+RobertsonBA1998@users.noreply.github.com>
2025-03-21 19:25:39 -0400

    pages_and_links

---
+++ app/styles/styles.tsx
@@ -0,0 +1,61 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+ container: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#fff",
+  paddingHorizontal: 20,
+ },
+
+ title: {
+  fontSize: 32,
+  marginBottom: 40,
+  fontWeight: "bold",
+  color: "black",
+ },
+
+ inputContainer: {
+  flexDirection: "row",
+  alignItems: "center",
+  width: "100%",
+  height: 50,
+  backgroundColor: "#f1f1f1",
+  borderRadius: 8,
+  paddingHorizontal: 10,
+  marginBottom: 20,
+ },
+
+ input: {
+  flex: 1,
+  height: "100%",
+ },
+
+ forgotPassword: {
+  alignSelf: "flex-end",
+  marginBottom: 20,
+  color: "#000",
+ },
+ button: {
+  width: "100%",
+  height: 50,
+  backgroundColor: "#1E90FF",
+  borderRadius: 8,
+  justifyContent: "center",
+  alignItems: "center",
+  marginBottom: 20,
+ },
+ buttonText: {
+  color: "#fff",
+  fontSize: 18,
+ },
+ signUp: {
+  color: "#000",
+ },
+ signUpLink: {
+  color: "#1E90FF",
+ },
+});
+
+export default styles;
